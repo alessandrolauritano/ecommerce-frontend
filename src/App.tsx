7b34aa7facbdc205990677e66071fd3190c1297d
Alessandro <alfeb@hotmail.it>
2023-11-08 17:26:13 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { Routes, Route } from "react-router-dom"
 import { Container } from "react-bootstrap"
+import { Routes, Route } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
 import { About } from "./pages/About"
 import { Navbar } from "./components/Navbar"
-import { ShoppingCartProvider } from "./ context/ShoppingCartContext"
+// import { ShoppingCartProvider } from "./ context/ShoppingCartContext"
 
 function App() {
   
 
   return (
-    <ShoppingCartProvider>
-    <Navbar/>
-    <Container className="mb-4">
+<>
+<Navbar/>
+  <Container className="mb-4">      
     <Routes>
-     <Route path="/" element = {<Home/>} />
-     <Route path="/about" element = {<About/>} />
-     <Route path="/store" element = {<Store/>} />
-     </Routes>
-    </Container>
-    </ShoppingCartProvider>
+        <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/store" element={<Store/>}/>
+      </Routes>
+  </Container>
+</>  
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,21 @@
-import { Container, Nav, Button, Navbar as NavbarBs} from "react-bootstrap" //bs navbar
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap"
 import { NavLink } from "react-router-dom"
-import { useShoppingCart } from "../ context/ShoppingCartContext"
+import { AiOutlineShoppingCart } from "react-icons/ai"
 
-
-export function Navbar() {
-    const { cartQuantity } = useShoppingCart();
-
-    return ( 
-        <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+export function Navbar(){
+    return (
+        <NavbarBs className="bg-white mb-3 shadow-sm">
             <Container>
-                <Nav className ="me-auto" >
-                    <Nav.Link to={"/"} as = {NavLink}>Home</Nav.Link>
-                    <Nav.Link to={"/about"} as = {NavLink}>About</Nav.Link>
-                    <Nav.Link to={"/store"} as = {NavLink}>Store</Nav.Link>
-              </Nav>
-              <Button style = {{ width: "3rem", height: "3rem", position: "relative"}} variant = "outline-primary">
-                <div className="bg-danger d-flex justify-content-center align-items-center rounded-circle" 
-                     style={{ 
-                     color: "white",
-                     width: "1.5rem", 
-                     height: "1.5rem", 
-                     position: "absolute", 
-                     bottom: 0,
-                     right: 0,
-                     transform: "translate(50%, 20%)"
-                     }}>{ cartQuantity }</div>
-              </Button>
+            <Nav>
+                <Nav.Link to="/" as={ NavLink }>Home</Nav.Link>
+                <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
+                <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
+            </Nav>
+            <Button
+            style={{ width: "3rem", height: "3rem"}} className="p-0">    
+            <AiOutlineShoppingCart size={30}/>            
+    
+    </Button> 
             </Container>
         </NavbarBs>
     )
